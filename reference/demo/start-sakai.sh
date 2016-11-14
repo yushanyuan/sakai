ce1374a1def947bf134f18486876d6113509f978
yushanyuan <yushanyuan@163.com>
2016-11-14 09:57:56 +0800

    start-sakai.sh

--- reference/demo/start-sakai.sh
+++ reference/demo/start-sakai.sh
@@ -1,4 +1,15 @@
 #!/bin/sh
+USER_DIR="/home/sakai"
+
+export LANG="zh_CN.UTF-8"
+export LC_ALL="zh_CN.UTF-8"
+
+export JAVA_HOME="$USER_DIR"/jdk1.8
+
+export CATALINA_HOME="$USER_DIR"/tomcat8
+
+export PATH=$JAVA_HOME/bin:$CATALINA_HOME/bin:$PATH:$HOME/bin
+
 
 #JAVA_OPTS="-server -Xmx1024m -XX:MaxNewSize=256m -XX:MaxPermSize=512m -Dhttp.agent=Sakai -Dsakai.demo=true -Djava.awt.headless=true -Dorg.apache.jasper.compiler.Parser.STRICT_QUOTE_ESCAPING=false -Dorg.apache.jasper.compiler.Parser.STRICT_WHITESPACE=false -Dsun.lang.ClassLoader.allowArraySyntax=true  -Dfile.encoding=utf-8 -Dsun.jnu.encoding=utf-8"
 JAVA_OPTS="-server -Xmx2024m -XX:MaxNewSize=256m -XX:MaxPermSize=512m -Dhttp.agent=Sakai -Djava.awt.headless=true -Dorg.apache.jasper.compiler.Parser.STRICT_QUOTE_ESCAPING=false -Dorg.apache.jasper.compiler.Parser.STRICT_WHITESPACE=false -Dsun.lang.ClassLoader.allowArraySyntax=true  -Dfile.encoding=utf-8 -Dsun.jnu.encoding=utf-8"

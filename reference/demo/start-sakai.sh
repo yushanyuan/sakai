f99d9317123ae67f3d119efdb5d5d06d569a7688
yushanyuan <yushanyuan@163.com>
2016-11-17 14:44:15 +0800

    添加地区参数，防止乱码

--- reference/demo/start-sakai.sh
+++ reference/demo/start-sakai.sh
@@ -12,11 +12,11 @@ export PATH=$JAVA_HOME/bin:$CATALINA_HOME/bin:$PATH:$HOME/bin
 
 
 #JAVA_OPTS="-server -Xmx1024m -XX:MaxNewSize=256m -XX:MaxPermSize=512m -Dhttp.agent=Sakai -Dsakai.demo=true -Djava.awt.headless=true -Dorg.apache.jasper.compiler.Parser.STRICT_QUOTE_ESCAPING=false -Dorg.apache.jasper.compiler.Parser.STRICT_WHITESPACE=false -Dsun.lang.ClassLoader.allowArraySyntax=true  -Dfile.encoding=utf-8 -Dsun.jnu.encoding=utf-8"
-JAVA_OPTS="-server -Xmx2024m -XX:MaxNewSize=256m -XX:MaxPermSize=512m -Dhttp.agent=Sakai -Djava.awt.headless=true -Dorg.apache.jasper.compiler.Parser.STRICT_QUOTE_ESCAPING=false -Dorg.apache.jasper.compiler.Parser.STRICT_WHITESPACE=false -Dsun.lang.ClassLoader.allowArraySyntax=true  -Dfile.encoding=utf-8 -Dsun.jnu.encoding=utf-8"
+JAVA_OPTS="-server -Xmx2024m -XX:MaxNewSize=256m -XX:MaxPermSize=512m -Dhttp.agent=Sakai -Djava.awt.headless=true -Dorg.apache.jasper.compiler.Parser.STRICT_QUOTE_ESCAPING=false -Dorg.apache.jasper.compiler.Parser.STRICT_WHITESPACE=false -Dsun.lang.ClassLoader.allowArraySyntax=true  -Dfile.encoding=utf-8 -Dsun.jnu.encoding=utf-8 -Duser.region=CN"
 export JAVA_OPTS
 
 #CATALINA_OPTS="-server -Xmx1024m -XX:MaxNewSize=256m -XX:MaxPermSize=512m -Dhttp.agent=Sakai -Dsakai.demo=true -Djava.awt.headless=true -Dorg.apache.jasper.compiler.Parser.STRICT_QUOTE_ESCAPING=false -Dorg.apache.jasper.compiler.Parser.STRICT_WHITESPACE=false -Dsun.lang.ClassLoader.allowArraySyntax=true  -Dfile.encoding=utf-8 -Dsun.jnu.encoding=utf-8"
-CATALINA_OPTS="-server -Xmx2024m -XX:MaxNewSize=256m -XX:MaxPermSize=512m -Dhttp.agent=Sakai -Djava.awt.headless=true -Dorg.apache.jasper.compiler.Parser.STRICT_QUOTE_ESCAPING=false -Dorg.apache.jasper.compiler.Parser.STRICT_WHITESPACE=false -Dsun.lang.ClassLoader.allowArraySyntax=true  -Dfile.encoding=utf-8 -Dsun.jnu.encoding=utf-8"
+CATALINA_OPTS="-server -Xmx2024m -XX:MaxNewSize=256m -XX:MaxPermSize=512m -Dhttp.agent=Sakai -Djava.awt.headless=true -Dorg.apache.jasper.compiler.Parser.STRICT_QUOTE_ESCAPING=false -Dorg.apache.jasper.compiler.Parser.STRICT_WHITESPACE=false -Dsun.lang.ClassLoader.allowArraySyntax=true  -Dfile.encoding=utf-8 -Dsun.jnu.encoding=utf-8 -Duser.region=CN"
 export CATALINA_OPTS
 
 # Demo failed to start correctly if CATALINA_HOME / CATALINA_BASE is set SAK-11553
